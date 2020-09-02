30c449d849a18a32f6079b7b3eda0a928b1376f4
Matthew Hartley <mhartley@cantab.net>
2020-09-02 10:40:38 +0100

    Add library code

---
+++ runtools/spec.py
@@ -0,0 +1,26 @@
+import json
+import pathlib
+
+from types import SimpleNamespace
+
+
+class ItemSpec(SimpleNamespace):
+
+    def template_repr(self, template):
+        return template.format(**self.__dict__)
+
+
+def item_spec_from_fpath(fpath):
+    with open(fpath) as fh:
+        metadata = json.load(fh)
+
+    return ItemSpec(**metadata["spec"])
+
+
+def get_all_specs(dirpath):
+
+    fpath_iter = dirpath.rglob("*.json")
+    specs = [item_spec_from_fpath(fpath) for fpath in fpath_iter]
+
+    return specs
+
