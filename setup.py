5c6b5c004ed07f1fbc2631446587e83d25577605
Matthew Hartley <mhartley@cantab.net>
2021-02-02 15:08:39 +0000

    Add setup and ignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='runtools',
+      version='0.1',
+      description='data prep',
+      url='https://github.com/mrmh2/runtools',
+      author='Matthew Hartley',
+      author_email='Matthew.Hartley@jic.ac.uk',
+      license='MIT',
+      packages=['runtools'],
+      zip_safe=False)
